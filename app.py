daa36419874cf5baa7a3337ca2db46b04e7353a4
Gamafoot <dimondima762@gmail.com>
2022-01-04 02:29:48 +0900

    Work with a form
    Auto submit after select value in form

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def main():
+    if request.method == 'GET':
+        print(request.args.get('comp_select'))
     return render_template('index.html')
 
 if __name__ == '__main__':
