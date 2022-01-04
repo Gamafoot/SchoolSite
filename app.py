3bd6f760d554761997be4712b6639b678687976a
Gamafoot <dimondima762@gmail.com>
2022-01-04 16:14:33 +0900

    Data Base
    Created DB

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, request
+import sqlite3 as sql
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/', methods=['GET'])
+conn = sql.connect('db.sqlite3')
+conn.cursor().execute('SELECT * FROM teachers')
+data = conn.cursor().fetchall()
+print('ДАТАААААА',data)
+
+@app.route('/', methods=['POST', 'GET'])
 def main():
-    if request.method == 'GET':
-        print(request.args.get('comp_select'))
+    if request.method == 'POST':
+        number = request.form['comp_select']
+    
+    #----------------------------------------#
+
+    data = {
+        # 'class': number,
+    }
     return render_template('index.html')
 
 if __name__ == '__main__':
