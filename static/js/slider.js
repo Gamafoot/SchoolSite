b761d547722412b73a19c57b8a15cb7e4df2b89e
Gamafoot <dimondima762@gmail.com>
2022-02-09 02:00:33 +0900

    Making slider

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,7 +1,8 @@
-new Swiper('.sc',{
+new Swiper('.swiper-container',{
+    slidesPerView: 2,
+    spaceBetween: 100,
     navigation:{
-        nextEl:'.swiper-button-next',
-        prevEl:'.swiper-button-prev'
+        nextEl:'swiper-button-next',
+        prevEl:'swiper-button-prev',
     },
-    loop: true,
 });
