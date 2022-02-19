e6aa857d892ba27ce9ab59136f96f8323d949d60
Gamafoot <dimondima762@gmail.com>
2022-02-19 11:53:07 +0900

    Made cool slider

--- static/js/home/gallery.js
+++ static/js/home/gallery.js
@@ -1,16 +1,22 @@
-$(()=>{
-    new Swiper('.gallery-swiper', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        thumbs:{
-            swiper:{
-                el:'.small-swiper',
-                slidesPerView: 5,
-            }
-        }
-    })
+let posts = $('#posts');
+let btn_back = $('#back');
+let gallery_swiper = $('.gallery-swiper');
 
-    
-});
+function OpenPost(index){
+    posts.css('display', 'none')
+    btn_back.css('display','')
+    gallery_swiper.eq(index).css('display', '')
+    for(let i = 0; i < gallery_swiper.length; i++){
+        if(i != index){
+            gallery_swiper.eq(i).css('display', 'none');
+        }
+    }
+}
+
+function BackToPanel(){
+    posts.css('display', '')
+    btn_back.css('display','none')
+    for(let i = 0; i < gallery_swiper.length; i++){
+        gallery_swiper.eq(i).css('display', 'none');
+    }
+}
